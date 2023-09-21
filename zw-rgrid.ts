977b9fb7ad6532c6b35faa790d7d563b46af8ff8
Pedro Alonso <alonso@zonaweb.com.br>
2023-09-21 02:06:13 -0300

    code optimizations

--- zw-rgrid.ts
+++ zw-rgrid.ts
@@ -1,76 +1,91 @@
 // Add an event listener to run code when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
-
     // Find all tables with class "zw-rgrid" and process them
-    document.querySelectorAll("table.zw-rgrid").forEach(function (item, index) {
-        zw_rgridfy(item);
+    document.querySelectorAll("table.zw-rgrid").forEach(function (item) {
+        processTable(item);
     })
 
     // Attach a click event listener to expand/contract ".zw-rgrid-header-mobile" elements with hidden content
-    document.querySelectorAll(".zw-rgrid-header-mobile").forEach(function (item, index) {
+    document.querySelectorAll(".zw-rgrid-header-mobile").forEach(function (item) {
         item.addEventListener("click", function () {
-            this.closest('tr').classList.toggle("expanded");
+            toggleExpanded(this);
         })
     });
-
 }, false);
 
-// Function to enhance tables with class "zw-rgrid"
-function zw_rgridfy(table) {
-
-    // Select all header cells in the table
-    var table_headers = table.querySelectorAll("thead tr th");
-
-    // Select all rows in the table
-    var table_rows = table.querySelectorAll("tbody tr");
-
-    // Flag to check if there is a primary key column
-    var haspk = false;
-
-    // Check if any header cell has a "data-pk" attribute
-    table_headers.forEach((obj) => {
-        if (obj.dataset.pk) {
-            haspk = true;
-        }
-    });
+// Function to process a table with class "zw-rgrid"
+function processTable(table: Element) {
+    const table_headers = table.querySelectorAll("thead tr th");
+    const table_rows = table.querySelectorAll("tbody tr");
+    let has_pk = hasPrimaryKey(table_headers);
 
     // Process each row in the table
-    table_rows.forEach((obj) => {
-        // Select all cells in the row (excluding mobile header cells)
-        var table_cells = obj.querySelectorAll('td:not(.zw-rgrid-header-mobile)');
-        var PkValues = [];
+    table_rows.forEach((row) => {
+        const table_cells = row.querySelectorAll('td:not(.zw-rgrid-header-mobile)');
+        let PkValues = [];
 
-        // If there is a primary key column, extract the values
-        if (haspk) {
-            table_headers.forEach((obj, index) => {
-                if (obj.dataset.pk && table_cells.length > 0) {
-                    PkValues.push(table_cells[index].innerText);
-                }
-            });
+        if (has_pk) {
+            PkValues = extractPrimaryKeyValues(table_headers, table_cells);
 
             // Insert a mobile header cell with concatenated primary key values
-            obj.insertBefore(createHeaderMobile(PkValues), table_cells[0]);
+            insertMobileHeaderCell(PkValues, row);
         }
 
-        // Set "data-th" attribute and add "zw-rgrid-detail" class for certain cells
-        table_cells.forEach((obj, index) => {
-            obj.setAttribute("data-th", table_headers[index].innerText);
-            if (table_headers[index].dataset.hide) {
-                obj.classList.add('zw-rgrid-detail');
-            }
-        });
-
+        setAttributesAndClasses({table_cells: table_cells, table_headers: table_headers});
     });
 }
 
+// Function to check if there is a primary key column in the table headers
+function hasPrimaryKey(table_headers: NodeListOf<Element>) {
+    return Array.from(table_headers).some((obj: { dataset: { pk: any; }; }) => obj.dataset.pk);
+}
+
+// Function to extract primary key values from the table
+function extractPrimaryKeyValues(table_headers: NodeListOf<Element>, table_cells: string | any[] | NodeListOf<Element>) {
+    return Array.from(table_headers).reduce((values: any[], obj: {
+        dataset: { pk: any; };
+    }, index: string | number) => {
+        if (obj.dataset.pk && table_cells.length > 0) {
+            values.push(table_cells[index].innerText);
+        }
+        return values;
+    }, []);
+}
+
+// Function to insert a mobile header cell with concatenated primary key values
+function insertMobileHeaderCell(PkValues: any[], row: Element) {
+    const td_header_mobile = createHeaderMobile(PkValues);
+    row.insertBefore(td_header_mobile, row.querySelector('td:not(.zw-rgrid-header-mobile)'));
+}
+
 // Function to create a mobile header cell with concatenated primary key values
-function createHeaderMobile(strPkValue: string[]) {
-
-    var td_header_mobile;
-
-    td_header_mobile = document.createElement("td");
+function createHeaderMobile(strPkValue: any[]) {
+    const td_header_mobile = document.createElement("td");
     td_header_mobile.classList.add('zw-rgrid-header-mobile');
     td_header_mobile.innerText = strPkValue.join(", ");
-
     return td_header_mobile;
+}
+
+// Function to set "data-th" attribute and add "zw-rgrid-detail" class for certain cells
+function setAttributesAndClasses({table_cells, table_headers}: { table_cells: any, table_headers: any }) {
+    Array.from(table_cells).forEach((cell: {
+        setAttribute: (arg0: string, arg1: any) => void;
+        classList: { add: (arg0: string) => void; };
+    }, index: string | number) => {
+        cell.setAttribute("data-th", table_headers[index].innerText);
+        if (table_headers[index].dataset.hide) {
+            cell.classList.add('zw-rgrid-detail');
+        }
+    });
+}
+
+// Function to toggle the "expanded" class on the closest table row
+function toggleExpanded(element: {
+    closest: (arg0: string) => {
+        (): any;
+        new(): any;
+        classList: { (): any; new(): any; toggle: { (arg0: string): void; new(): any; }; };
+    };
+}) {
+    element.closest('tr').classList.toggle("expanded");
 }
